a271980b4973e2380b5bc355472362338c9a0452
Scott W Houghton <scott.houghton@live.com>
2022-07-13 12:23:52 -0400

    Test flatten

--- modules/networking/output.tf
+++ modules/networking/output.tf
@@ -7,7 +7,7 @@ output "public_subnets_id" {
 }
 
 output "private_subnets_id" {
-  value = toset([aws_subnet.private_subnet.*.id])
+  value = toset([flatten([aws_subnet.private_subnet.*.id])])
 }
 
 output "default_sg_id" {
