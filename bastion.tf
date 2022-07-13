cdb052f9f40e76a58c9bf56e469faef09595c657
Scott W Houghton <scott.houghton@live.com>
2022-07-13 11:48:14 -0400

    Test output flatten

--- bastion.tf
+++ bastion.tf
@@ -1,8 +1,8 @@
-module "bastion" {
-  for_each = flatten([ module.networking.private_subnets_id])
-  source            = "github.com/jetbrains-infra/terraform-aws-bastion-host"
-  subnet_id         = module.networking.public_subnets_id[index(keys(module.networking.private_subnets_id), each.key)]
-  ssh_key           = var.bastion_key_pair
-  internal_networks = [each.key]
-  project           = "myProject"
-}
+# module "bastion" {
+#   for_each = flatten([ module.networking.private_subnets_id])
+#   source            = "github.com/jetbrains-infra/terraform-aws-bastion-host"
+#   subnet_id         = module.networking.public_subnets_id[index(keys(module.networking.private_subnets_id), each.key)]
+#   ssh_key           = var.bastion_key_pair
+#   internal_networks = [each.key]
+#   project           = "myProject"
+# }
