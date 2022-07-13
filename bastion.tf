51b4094acd9bcd47f8aac6a9b18c846bf47fbeb9
Scott W Houghton <scott.houghton@live.com>
2022-07-13 10:34:57 -0400

    Fix string

--- bastion.tf
+++ bastion.tf
@@ -3,6 +3,6 @@ module "bastion" {
   source            = "github.com/jetbrains-infra/terraform-aws-bastion-host"
   subnet_id         = module.networking.public_subnets_id[index(keys(module.networking.private_subnets_id), each.key)]
   ssh_key           = var.bastion_key_pair
-  internal_networks = each.key
+  internal_networks = [each.key]
   project           = "myProject"
 }
