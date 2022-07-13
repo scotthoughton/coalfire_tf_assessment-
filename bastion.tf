2464574e5f404f6f1a5c6ea1f9d0054a32540216
Scott W Houghton <scott.houghton@live.com>
2022-07-13 15:13:47 -0400

    Test bastion

--- bastion.tf
+++ bastion.tf
@@ -1,21 +1,10 @@
-locals {
-  networks = flatten([
-    for private_network_key, private_network in module.networking.private_subnets_id : [
-      for public_network_key, public_network in module.networking.public_subnets_id : {
-        private_network_key = private_network_key
-        public_network_key = public_network_key
-      }
-    ]
-  ])
-}
+
 
 module "bastion" {
-  for_each          = {
-    for network in local.networks : "${public_network_key.private_network_key}.${public_network_key.public_network_key}" => network
-  } 
+  for_each          = module.networking.public_subnets_id
   source            = "github.com/jetbrains-infra/terraform-aws-bastion-host"
-  subnet_id         = module.networking.public_subnets_id[each.key.private_network_key]
+  subnet_id         = each.key
   ssh_key           = var.bastion_key_pair
-  internal_networks = module.networking.private_subnets_id[each.key.private_network_key]
+  internal_networks = module.networking.private_subnets_id[each.key.index]
   project           = "myProject"
 }
