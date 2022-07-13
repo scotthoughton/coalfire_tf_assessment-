7dae5da93113257e69ddd246355aa1cc0e8b5681
Scott W Houghton <scott.houghton@live.com>
2022-07-13 16:30:06 -0400

    Added EKS

--- bastion.tf
+++ bastion.tf
@@ -2,7 +2,7 @@
 
 module "bastion" {
   count             = length(module.networking.public_subnets_id)
-  source            = "github.com/jetbrains-infra/terraform-aws-bastion-host"
+  source            = "./modules/bastion"
   subnet_id         = module.networking.public_subnets_id[count.index]
   ssh_key           = var.bastion_key_pair
   internal_networks = var.private_subnets_cidr
---
+++ eks.tf
@@ -0,0 +1,87 @@
+### EKS IAM ###
+resource "aws_iam_role" "EKSClusterRole" {
+  name = "EKSClusterRole"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role" "NodeGroupRole" {
+  name = "EKSNodeGroupRole"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.EKSClusterRole.name
+}
+resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.NodeGroupRole.name
+}   
+resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.NodeGroupRole.name
+}
+resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.NodeGroupRole.name
+}
+###
+resource "aws_eks_cluster" "eks-cluster" {
+  name     = "eks-cluster"
+  role_arn = aws_iam_role.EKSClusterRole.arn
+  version  = "1.21"
+
+  vpc_config {
+    subnet_ids          = flatten([ module.networking.public_subnets_id, module.networking.private_subnets_id ])
+    security_group_ids  = flatten(module.networking.security_groups_id)
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.AmazonEKSClusterPolicy
+  ]
+}
+
+resource "aws_eks_node_group" "node-ec2" {
+  cluster_name    = aws_eks_cluster.eks-cluster.name
+  node_group_name = "t3_micro-node_group"
+  node_role_arn   = aws_iam_role.NodeGroupRole.arn
+  subnet_ids      = flatten( module.networking.private_subnets_id )
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 3
+    min_size     = 1
+  }
+
+  ami_type       = "AL2_x86_64"
+  instance_types = ["t3.micro"]
+  capacity_type  = "ON_DEMAND"
+  disk_size      = 20
+
+  depends_on = [
+    aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
+    aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy
+  ]
+}
