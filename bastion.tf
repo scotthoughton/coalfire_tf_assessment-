abfe945c0e0fc7d5c2e09d00afdd0bc15b843b00
Scott W Houghton <scott.houghton@live.com>
2022-07-13 15:47:19 -0400

    Fixed length

--- bastion.tf
+++ bastion.tf
@@ -1,7 +1,7 @@
 
 
 module "bastion" {
-  count             = length([module.networking.public_subnets_id])
+  count             = length(module.networking.public_subnets_id)
   source            = "github.com/jetbrains-infra/terraform-aws-bastion-host"
   subnet_id         = module.networking.public_subnets_id[count.index]
   ssh_key           = var.bastion_key_pair
